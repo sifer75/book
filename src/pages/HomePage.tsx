f9dfaf2dab31ab99274c06a84bfee8f9393658f3
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-12 22:57:05 +0200

    fix: update HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import FadingSkills from "@/components/Fragments/FadingSkills";
 import Header from "@/components/Header/Header";
 import Menu from "@/components/Header/Menu/Menu";
 
@@ -9,10 +10,16 @@ function HomePage({ id }: HomePageProps) {
   return (
     <div
       id={`HomePage__container__${id}`}
-      className="w-full h-full"
+      className="w-full h-[2000px] relative bg-red-500"
     >
       <Menu id={`HomePage__menu__${id}`} />
       <Header id={`HomePage__header__${id}`} />
+      <div
+        id={`FadingSkills__container__${id}`}
+        className="absolute top-180 right-10"
+      >
+        <FadingSkills id={`HomePage__fadingSkills__${id}`} />
+      </div>
     </div>
   );
 }
