3b3d7e4f69f0b39217539b88ab227146a5bc4111
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-12 15:33:43 +0200

    fix: update HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,20 @@
-import { ModeToggle } from "@/components/mode-toggle";
+import Header from "@/components/Header/Header";
+import Menu from "@/components/Header/Menu/Menu";
 
-function HomePage() {
-  return( <ModeToggle />)
+interface HomePageProps {
+  id: string;
+}
+
+function HomePage({ id }: HomePageProps) {
+  return (
+    <div
+      id={`HomePage__container__${id}`}
+      className="w-full h-full"
+    >
+      <Menu id={`HomePage__menu__${id}`} />
+      <Header id={`HomePage__header__${id}`} />
+    </div>
+  );
 }
 
 export default HomePage;
