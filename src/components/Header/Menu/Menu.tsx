348363de10e958cdd40cda35777720901b4d7307
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-12 14:33:10 +0200

    feat: create Menu container

---
+++ src/components/Header/Menu/Menu.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef, useState } from "react";
+import MenuButtons from "./MenuButtons";
+import MenuToggle from "./MenuToggle";
+
+interface MenuBarProps {
+  id: string;
+}
+
+function Menu({ id }: MenuBarProps) {
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setMenuIsOpen(false);
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div id={`Menu__container__${id}`} ref={menuRef}>
+      <MenuToggle
+        id={`Menu__menuToggle__${id}`}
+        menuIsOpen={menuIsOpen}
+        setMenuIsOpen={setMenuIsOpen}
+      />
+      <div
+        id={`Menu__menuButtons__container__${id}`}
+        className={`flex absolute flex-col transition-all duration-700 overflow-hidden ease-in-out bg-white ${
+          menuIsOpen
+            ? "h-screen w-60"
+            : "h-16 w-16 rounded-[48px] ml-5 mt-4 hover:scale-125"
+        }`}
+      >
+        <MenuButtons
+          id={`Menu__menuButtons__${id}`}
+          menuIsOpen={menuIsOpen}
+          setMenuIsOpen={setMenuIsOpen}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
