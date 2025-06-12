e1cdd94a288d097eb1e4c99d45ae501bfc385c10
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-12 15:31:45 +0200

    fix: update routing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./index.css";
 import HomePage from "./pages/HomePage";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage id="HomePage" />} />
+      </Routes>
     </ThemeProvider>
   );
 }
