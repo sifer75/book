aed7b5f194b70a52e7e02f325aa561cbe579f6fe
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-23 11:00:11 +0200

    fix: update MyHeadline

--- src/components/Fragments/MyHeadline.tsx
+++ src/components/Fragments/MyHeadline.tsx
@@ -8,17 +8,17 @@ function MyHeadline({ id }: MyHeadLineProps) {
   return (
     <div
       id={`MyHeadline__container__${id}`}
-      className="w-full xs:flex xs:items-center h-fit transition duration-500 delay-100 flex flex-col items-center sm:items-start"
+      className="w-full xs:flex xs:items-center h-fit transition duration-500 delay-100 flex flex-col items-center sm:items-start z-30"
     >
       <h1
         id={`MyHeadline__texts__${id}`}
-        className="font-Merich w-fit z-20 text-yellow-100"
+        className="font-merich w-fit z-20 text-yellow-100"
       >
         {me.map((txt, k) => (
           <div
             id={`MyHeadline__text__${txt}__${id}`}
-            key={k}
-            className="text-7xl z-20 xs:text-8xl lg:text-9xl"
+            key={`${txt}-${k}`}
+            className="text-7xl xs:text-8xl lg:text-9xl"
           >
             {txt}
           </div>
