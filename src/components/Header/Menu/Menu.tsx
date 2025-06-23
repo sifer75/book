e40c8397eccda6fcd234ada637aa8504cbcc2fe9
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-23 11:00:43 +0200

    fix: update Menu

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -23,7 +23,11 @@ function Menu({ id }: MenuBarProps) {
   }, []);
 
   return (
-    <div id={`Menu__container__${id}`} ref={menuRef}>
+    <div
+      id={`Menu__container__${id}`}
+      ref={menuRef}
+      className="w-fit h-fit z-20 top-5 left-5 flex items-center gap-5"
+    >
       <MenuToggle
         id={`Menu__menuToggle__${id}`}
         menuIsOpen={menuIsOpen}
@@ -31,7 +35,7 @@ function Menu({ id }: MenuBarProps) {
       />
       <div
         id={`Menu__menuButtons__container__${id}`}
-        className={`flex absolute flex-col transition-all duration-700 overflow-hidden ease-in-out bg-white ${
+        className={`flex flex-col transition-all duration-700 overflow-hidden ease-in-out bg-white z-10 ${
           menuIsOpen
             ? "h-screen w-60"
             : "h-16 w-16 rounded-[48px] ml-5 mt-4 hover:scale-125"
