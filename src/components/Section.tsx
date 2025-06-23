6a055a4fe6d61634c429aeb26bcd02ce6a2fefa4
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-24 00:33:38 +0200

    fix: update style Section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,13 +3,16 @@ import type { ReactNode } from "react";
 interface SectionProps {
   id: string;
   children: ReactNode;
+  viewHeight?: boolean;
 }
 
-function Section({ id, children }: SectionProps) {
+function Section({ id, children, viewHeight }: SectionProps) {
   return (
     <div
       id={`Section__container__${id}`}
-      className="flex flex-col w-screen h-screen scroll-smooth bg-blue-500 items-between p-20"
+      className={`flex flex-col w-screen h-fit scroll-smooth bg-blue-500 items-between p-20 ${
+        viewHeight ? "h-fit" : "h-screen"
+      }`}
     >
       {children}
     </div>
