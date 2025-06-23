8a7c4fbdb8f59bb8fbea33975b4002f25ce6f04a
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-23 11:24:55 +0200

    fix: update Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import SocialLinks from "./SocialLinks/SocialLinks";
-import Theming from "./Theming";
+import SiteToolbar from "@/components/Header/SiteToolbar";
+import Menu from "@/components/Header/Menu/Menu";
+import FontIcons from "@/components/FontIcons";
 
 interface HeaderProps {
   id: string;
@@ -7,12 +8,17 @@ interface HeaderProps {
 
 function Header({ id }: HeaderProps) {
   return (
-    <div
-      id={`Header__container__${id}`}
-      className="w-fit h-fit z-20 top-5 right-5 lg:top-10 lg:right-12 fixed flex items-center gap-5"
-    >
-      <SocialLinks id={`Header__socialLinks__${id}`} />
-      <Theming />
+    <div id={`Header__container__${id}`}>
+      <SiteToolbar id={`Header__siteToolbar__${id}`} />
+      <div id={`Header__menu__container__${id}`} className="h-full bg-blue-500">
+        <Menu id={`Header__menu__${id}`} />
+      </div>
+      <div
+        id={`Header__fontIcons__container__${id}`}
+        className="z-0 inset-0 absolute lg:block"
+      >
+        <FontIcons id={`Header__fontIcons__${id}`} />
+      </div>
     </div>
   );
 }
