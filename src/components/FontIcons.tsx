968a56246e0f59261c38e08d88086691b803c3a6
Fabien Taupin <taupinfabien2607@gmail.com>
2025-06-23 11:07:32 +0200

    fix: update FontIcons

--- src/components/FontIcons.tsx
+++ src/components/FontIcons.tsx
@@ -2,6 +2,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { logos } from "../lib/logo.utils";
 import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
+interface FontIconsProps {
+  id: string;
+}
+
 const shuffleArray = (array: IconDefinition[]) => {
   return array.sort(() => Math.random() - 0.5);
 };
@@ -17,12 +21,22 @@ const renderIcons = () => {
     />
   ));
 };
-function FontIcons() {
+function FontIcons({ id }: FontIconsProps) {
   return (
-    <div className="w-screen overflow-hidden h-screen text-3xl border border-black relative">
-      <div className="w-full h-full flex -left-[13%] relative items-center justify-center flex-col gap-10">
+    <div
+      id={`FontIcons__container__${id}`}
+      className="w-screen overflow-hidden h-screen text-3xl border border-black relative"
+    >
+      <div
+        id={`FontIcons__logo__container__${id}`}
+        className="w-full h-full flex -left-[13%] relative items-center justify-center flex-col gap-10"
+      >
         {[...Array(45)].map((_, index) => (
-          <div key={index} className="-rotate-45 flex gap-5 min-h-fit w-full">
+          <div
+            id={`FontIcons__logo__${index}__${id}`}
+            key={index}
+            className="-rotate-45 flex gap-5 min-h-fit w-full"
+          >
             {renderIcons()}
           </div>
         ))}
